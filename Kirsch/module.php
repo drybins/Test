5c03f51a96f0898a10465216d5028ad8e2be3e06
dierkrybinski <33806771+dierkrybinski@users.noreply.github.com>
2021-01-13 11:35:42 +0100

    Update module.php

--- Kirsch/module.php
+++ Kirsch/module.php
@@ -9,7 +9,16 @@ class BHKW extends IPSModule
         // Diese Zeile nicht löschen.
         parent::Create();
 	
-	// 1 - Profilname
+
+	//$this->ConnectParent("{3CFF0FD9-E306-41DB-9B5A-9D06D38576C3}");
+    }
+
+    // Überschreibt die intere IPS_ApplyChanges($id) Funktion
+    public function ApplyChanges()
+    {
+        // Diese Zeile nicht löschen
+        parent::ApplyChanges();
+   	// 1 - Profilname
 	// 2 - ProfilTyp 0 - Boolean, 1 - Integer, 2- Float, 3 - String 
 	// 3 - Profil Suffix
 	// 4 - MinValue
@@ -22,16 +31,7 @@ class BHKW extends IPSModule
 	$this->IPS_CreateVariableProfile("Status", 1, " Kw", 1, 11, 1, 2, "");
 	    $this->IPS_SetVariableProfileAssociation("Status", 1, "gestoppet", "", "LawnGreen");
 	$this->IPS_CreateVariableProfile("UpM", 1, " UpM", 0, 0, 1, 0, "");
-	//$this->RegisterVariableFloat("AnalogOut1", "Analog Out1", "Dierk");
-	//$this->ConnectParent("{3CFF0FD9-E306-41DB-9B5A-9D06D38576C3}");
-    }
-
-    // Überschreibt die intere IPS_ApplyChanges($id) Funktion
-    public function ApplyChanges()
-    {
-        // Diese Zeile nicht löschen
-        parent::ApplyChanges();
-        
+	//$this->RegisterVariableFloat("AnalogOut1", "Analog Out1", "Dierk");     
     
     }
     
