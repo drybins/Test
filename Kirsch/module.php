518eab1ffc78f161cfdfdc9659b00836d11cd40d
dierkrybinski <33806771+dierkrybinski@users.noreply.github.com>
2021-01-17 17:17:32 +0100

    Update module.php

--- Kirsch/module.php
+++ Kirsch/module.php
@@ -1,6 +1,6 @@
 <?php
 // Klassendefinition
-class BHKW extends IPSModule
+class KirschBHKW extends IPSModule
 {
 
     // Überschreibt die interne IPS_Create($id) Funktion
@@ -8,18 +8,30 @@ class BHKW extends IPSModule
     {
         // Diese Zeile nicht löschen.
         parent::Create();
-
-        //if(!IPS_VariableProfileExists("Dierk")) {
-        //    IPS_CreateVariableProfile("Dierk", 1);
-        //}
-        //if(!IPS_VARIABLEPROFILEEXISTS("Kw")
-        //{
-        //    IPS_CreateVariableProfile("Kw",1);
-        //}
-	//$this->CreateVariableProfile("ESERA.Spannung15V", 2, " V", 0, 15, 0.1, 2, "");
-        $this->IPS_CreateVariableProfile("Dierk", 2, " V", 0, 15, 0.1, 2, "";
-	$this->RegisterVariableFloat("AnalogOut", "Analog Out", "ESERA.Spannung10V");
-	//$this->ConnectParent("{3CFF0FD9-E306-41DB-9B5A-9D06D38576C3}");
+	
+	    $this->RegisterPropertyInteger("BHKW_Port", 7603);
+	       	// 1 - Profilname
+	// 2 - ProfilTyp 0 - Boolean, 1 - Integer, 2- Float, 3 - String 
+	// 3 - Profil Suffix
+	// 4 - MinValue
+	// 5 - MaxValue 
+	// 6 - Stepsize
+	// 7 - Nachkommastellen
+	// 8 - Icon
+	    
+        $this->IPS_CreateVariableProfile("Kirsch.Kw", 1, " Kw", 0, 0,1, 2, "");
+	$this->IPS_CreateVariableProfile("Kirsch.Status", 1, "", 1, 11, 1, 2, "");
+	    IPS_SetVariableProfileAssociation("Kirsch.Status", 1, "gestoppet", "", 0x7cfc00);
+	    IPS_SetVariableProfileAssociation("Kirsch.Status", 2, "startet", "", 0x7cfc00);
+	    IPS_SetVariableProfileAssociation("Kirsch.Status", 3, "aufwärmen", "", 0x7cfc00);
+	    IPS_SetVariableProfileAssociation("Kirsch.Status", 4, "läuft", "", 0x7cfc00);
+	    IPS_SetVariableProfileAssociation("Kirsch.Status", 5, "abkühlen", "", 0x7cfc00);
+	    IPS_SetVariableProfileAssociation("Kirsch.Status", 10, "Notstop", "", 0xff0000);
+	    IPS_SetVariableProfileAssociation("Kirsch.Status", 11, "Fehler", "", 0xff0000);
+	$this->IPS_CreateVariableProfile("Kirsch.UpM", 1, " UpM", 0, 0, 1, 0, "");
+	//$this->RegisterVariableFloat("AnalogOut1", "Analog Out1", "Dierk");  
+	    $this->ConnectParent("{3CFF0FD9-E306-41DB-9B5A-9D06D38576C3}");
+	
     }
 
     // Überschreibt die intere IPS_ApplyChanges($id) Funktion
@@ -27,23 +39,40 @@ class BHKW extends IPSModule
     {
         // Diese Zeile nicht löschen
         parent::ApplyChanges();
-        
+   
+
     
     }
-    
-    
 
-    public function ReceiveData($JSONString)
+	public function Send(string $Text)
+		{
+			$this->SendDataToParent(json_encode(Array("DataID" => "{79827379-F36E-4ADA-8A95-5F8D1DC92FA9}", "Buffer" => $Text)));
+		}
+
+	public function ReceiveData($JSONString)
 		{
 			$data = json_decode($JSONString);
-			IPS_LogMessage("IOTest RECV", utf8_decode($data->Buffer));
-
-			//Parse and write values to our variables
-			
-			//Send response back to the splitter
-			return "OK from " . $this->InstanceID;
+			IPS_LogMessage("Device RECV", utf8_decode($data->Buffer));
 		}
-        private function CreateVariableProfile($ProfileName, $ProfileType, $Suffix, $MinValue, $MaxValue, $StepSize, $Digits, $Icon) 
+
+    
+ //       public function LogDataBHKW($LogText)
+//		{
+//		IPS_LogMessage("IOTest RECV", "BHKW Started");
+//		IPS_LogMessage("IOTest RECV", $LogText);
+//	}
+
+    //public function ReceiveData($JSONString)
+//		{
+//			$data = json_decode($JSONString);
+//			IPS_LogMessage("IOTest RECV", utf8_decode($data->Buffer));
+//
+//			//Parse and write values to our variables
+//			
+//			//Send response back to the splitter
+//			return "OK from " . $this->InstanceID;
+//		}
+        private function IPS_CreateVariableProfile($ProfileName, $ProfileType, $Suffix, $MinValue, $MaxValue, $StepSize, $Digits, $Icon) 
 	{
 		    if (!IPS_VariableProfileExists($ProfileName)) 
 		    {
