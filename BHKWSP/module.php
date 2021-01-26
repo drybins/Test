b1a385b2690f8c2220707c88e885c2d4566731a8
dierkrybinski <33806771+dierkrybinski@users.noreply.github.com>
2021-01-26 12:59:10 +0100

    Add files via upload

--- BHKWSP/module.php
+++ BHKWSP/module.php
@@ -35,7 +35,23 @@
 		{
 			$data = json_decode($JSONString);
 			//IPS_LogMessage("Splitter RECV", utf8_decode($data->Buffer));
-			$this->DRTEST(utf8_decode($data->Buffer));
+					//Kontrollieren ob Buffer leer ist.
+			$bufferData = $this->GetBuffer("DataBuffer");
+			$bufferData .= $data->Buffer;
+			IPS_LogMessage("Splitter bufferData", $bufferData);
+			
+			$bufferParts = explode("\r\n", $bufferData);
+
+		//Letzten Eintrag nicht auswerten, da dieser nicht vollständig ist.
+		if(sizeof($bufferParts) > 1) {
+			for($i=0; $i<sizeof($bufferParts)-1; $i++) 
+			{
+				IPS_LogMessage("Splitter bufferParts", $i . ":" .$bufferParts[$i]);
+				//$this->SendDebug("Data", $bufferParts[$i], 0);
+				//$this->AnalyseData($bufferParts[$i]);
+			}
+		}
+			//$this->DRTEST(utf8_decode($data->Buffer));
 			//$this->SendDataToChildren(json_encode(Array("DataID" => "{185A67F4-5748-3EE1-4EED-CAF56975F21B}", "Buffer" => $data->Buffer)));
 		}
 		
