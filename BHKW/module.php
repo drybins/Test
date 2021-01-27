d55ec2c3b7d976f475eb2971af35aae090424172
dierkrybinski <33806771+dierkrybinski@users.noreply.github.com>
2021-01-27 11:19:43 +0100

    Add files via upload

--- BHKW/module.php
+++ BHKW/module.php
@@ -1,25 +1,12 @@
 <?php
-	class BHKW extends IPSModule {
+	class BHKWSP extends IPSModule {
 
 		public function Create()
 		{
 			//Never delete this line!
 			parent::Create();
-			
-			    $this->IPS_CreateVariableProfile("Kirsch.UpM", 1, " UpM", 0, 0, 1, 0, "");
-				$this->IPS_CreateVariableProfile("Kirsch.Kw", 1, " Kw", 0, 0,1, 2, "");
-				$this->IPS_CreateVariableProfile("Kirsch.Status", 1, "", 1, 11, 1, 2, "");
-				IPS_SetVariableProfileAssociation("Kirsch.Status", 1, "gestoppet", "", 0x7cfc00);
-				IPS_SetVariableProfileAssociation("Kirsch.Status", 2, "startet", "", 0x7cfc00);
-				IPS_SetVariableProfileAssociation("Kirsch.Status", 3, "aufwärmen", "", 0x7cfc00);
-				IPS_SetVariableProfileAssociation("Kirsch.Status", 4, "läuft", "", 0x7cfc00);
-				IPS_SetVariableProfileAssociation("Kirsch.Status", 5, "abkühlen", "", 0x7cfc00);
-				IPS_SetVariableProfileAssociation("Kirsch.Status", 10, "Notstop", "", 0xff0000);
-				IPS_SetVariableProfileAssociation("Kirsch.Status", 11, "Fehler", "", 0xff0000);	
 
-				$this->RegisterVariableInteger("KirschStatus", "Status", "Kirsch.Status", 10);
-
-				$this->ConnectParent("{33B9B2D7-6BC5-1CF6-A86F-E76622A7FFB7}");
+			$this->RequireParent("{3CFF0FD9-E306-41DB-9B5A-9D06D38576C3}");
 		}
 
 		public function Destroy()
@@ -34,27 +21,77 @@
 			parent::ApplyChanges();
 		}
 
-		public function Send()
+		public function ForwardData($JSONString)
 		{
-			$this->SendDataToParent(json_encode(Array("DataID" => "{EB1230DE-C6EE-1739-07A1-3742EE4FB43F}")));
+			$data = json_decode($JSONString);
+			IPS_LogMessage("Splitter FRWD", utf8_decode($data->Buffer));
+
+			$this->SendDataToParent(json_encode(Array("DataID" => "{79827379-F36E-4ADA-8A95-5F8D1DC92FA9}", "Buffer" => $data->Buffer)));
+
+			return "String data for device instance!";
 		}
 
 		public function ReceiveData($JSONString)
 		{
 			$data = json_decode($JSONString);
-			IPS_LogMessage("Device RECV", utf8_decode($data->Buffer));
+			//IPS_LogMessage("Splitter RECV", utf8_decode($data->Buffer));
+					//Kontrollieren ob Buffer leer ist.
+			$bufferData = $this->GetBuffer("DataBuffer");
+			$bufferData .= $data->Buffer;
+			IPS_LogMessage("Splitter bufferData", $bufferData);
+			
+			$delimeter = '<?xml version="1.0" encoding="UTF-8"?>';
+			$bufferParts = explode($delimeter, $bufferData);	
+			//$bufferParts = explode("\r\n", $bufferData);
+
+		//Letzten Eintrag nicht auswerten, da dieser nicht vollständig ist.
+		if(sizeof($bufferParts) > 1) 
+			{
+				for($i=0; $i<sizeof($bufferParts)-1; $i++) 
+				{
+					IPS_LogMessage("Splitter bufferParts", $i . ":" . $bufferParts[$i]);
+					//$this->SendDebug("Data", $bufferParts[$i], 0);
+					$this->AnalyseData($delimeter . $bufferParts[$i]);
+				}
+			}
+			$bufferData = $bufferParts[sizeof($bufferParts)-1];
+
+			//Übriggebliebene Daten auf den Buffer schreiben
+			$this->SetBuffer("DataBuffer", $bufferData);
+			//$this->DRTEST(utf8_decode($data->Buffer));
+			//$this->SendDataToChildren(json_encode(Array("DataID" => "{185A67F4-5748-3EE1-4EED-CAF56975F21B}", "Buffer" => $data->Buffer)));
 		}
 		
-		        private function IPS_CreateVariableProfile($ProfileName, $ProfileType, $Suffix, $MinValue, $MaxValue, $StepSize, $Digits, $Icon) 
-	{
-		    if (!IPS_VariableProfileExists($ProfileName)) 
-		    {
-			       IPS_CreateVariableProfile($ProfileName, $ProfileType);
-			       IPS_SetVariableProfileText($ProfileName, "", $Suffix);
-			       IPS_SetVariableProfileValues($ProfileName, $MinValue, $MaxValue, $StepSize);
-			       IPS_SetVariableProfileDigits($ProfileName, $Digits);
-			       IPS_SetVariableProfileIcon($ProfileName, $Icon);
-		    }
-	 }
+		
+		public function AnalyseData(string $data)
+		{
+			IPS_LogMessage("Splitter data", $data);
+			$start = strpos($data,"<",5);
+			$end = strpos($data,">",$start);
+			$cmd = substr($data, $start+1, $end-$start-1);
+			IPS_LogMessage("Splitter CMD", $cmd);
 
+				//IPS_LogMessage("Splitter CMD", $cmd);
+				switch ($cmd)
+				{
+					case "statePP":
+						$this->statePP($data);
+						break;
+					default:
+						break;
+				}
+			}
+
+		
+		public function statePP(string $cmd)
+		{
+			IPS_LogMessage("Splitter statePP", $cmd);
+			
+			//$xmlData = @new SimpleXMLElement(utf8_encode($cmd), LIBXML_NOBLANKS + LIBXML_NONET);
+			//$SatusID = $this->GetIDForIdent("KirschStatus");
+			//IPS_LogMessage("Splitter statePP StatusID", $StatusID);
+			//$ScriptData['STATUS'] = (string) $xmlData->common[0]->state;
+			$data = $cmd;
+			$this->SendDataToChildren(json_encode(Array("DataID" => "{185A67F4-5748-3EE1-4EED-CAF56975F21B}", "Buffer" => utf8_encode($data))));
+		}
 	}
