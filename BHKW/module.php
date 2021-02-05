96e46727d5caa7698ed36569c118c4659a496033
dierkrybinski <33806771+dierkrybinski@users.noreply.github.com>
2021-02-05 18:44:21 +0100

    Add files via upload

--- BHKW/module.php
+++ BHKW/module.php
@@ -21,7 +21,10 @@
 				IPS_SetVariableProfileAssociation("Kirsch.Status", 10, "Notstop", "", 0xff0000);
 				IPS_SetVariableProfileAssociation("Kirsch.Status", 11, "Fehler", "", 0xff0000);	
 				$BHKWID = IPS_GetInstance($this->InstanceID)["InstanceID"];
-				IPS_LogMessage("BHKW ID", $BHKWID);
+				$CatID = IPS_CreateCategory();       // Kategorie anlegen
+				IPS_SetName($CatID, "statePP"); // Kategorie benennen
+				IPS_SetParent($CatID, $BHKWID);
+				//IPS_LogMessage("BHKW ID", $BHKWID);
 				//$Parent = IPS_GetParent();
 				//BHKW statePP Variablen anlegen
 				$this->RegisterVariableInteger("KirschStatus", "Status", "Kirsch.Status", 10);
