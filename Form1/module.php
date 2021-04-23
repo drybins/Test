020cfcf2b015be0558542b2bcc0996d4853d576f
dierkrybinski <33806771+dierkrybinski@users.noreply.github.com>
2021-04-23 10:19:33 +0200

    Add files via upload

--- Form1/module.php
+++ Form1/module.php
@@ -1,1 +1,50 @@
-<php
+<?php
+
+declare(strict_types=1);
+
+
+class DRForm extends ipsmodule
+{
+
+
+    /**
+     * Interne Funktion des SDK.
+     */
+    public function Create()
+    {
+        parent::Create();
+
+        $this->RegisterPropertyString('Host', '');
+        $this->RegisterPropertyString('Username', '');
+        $this->RegisterPropertyString('Password', '');
+        $this->RegisterPropertyString('Database', 'IPS');
+        $this->RegisterPropertyString('Variables', json_encode([]));
+        $this->RegisterTimer('LogData', 0, 'ACmySQL_LogData($_IPS[\'TARGET\']);');
+        $this->Vars = [];
+        $this->Buffer = [];
+    }
+
+    /**
+     * Interne Funktion des SDK.
+     */
+    public function ApplyChanges()
+    {
+        parent::ApplyChanges();
+
+    }
+
+    /**
+     * Interne Funktion des SDK.
+     */
+    public function GetConfigurationForm()
+    {
+        $form = json_decode(file_get_contents(__DIR__ . '/form.json'), true);
+
+        $ConfigVars = json_decode($this->ReadPropertyString('Variables'), true);
+ 
+        return json_encode($form);
+    }
+
+}
+
+/* @} */
